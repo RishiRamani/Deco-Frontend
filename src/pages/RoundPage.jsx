d8cadb55f17b123e352b97861a280f121edf28c4
Arnav Jain <arnav.1216403224@std.ggsipu.ac.in>
2026-05-10 18:53:43 +0530

    fixed padding in question box

--- src/pages/RoundPage.jsx
+++ src/pages/RoundPage.jsx
@@ -355,7 +355,7 @@ function QuestionCard({ question, questionKey, questionNumber, totalQuestions, o
         minHeight: isLandscapeMobile ? 'unset' : sceneTheme.questionBox.compactMinHeight || 'auto',
       }}
     >
-      <div className="flex flex-wrap items-start justify-between gap-4">
+      <div className="flex flex-wrap items-start justify-between gap-4 p-2">
         <div className="flex-1">
           <div className={questionNumberClass}>Question {questionNumber}</div>
           <h2
